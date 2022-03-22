f1a32b8132ababde3ac6bd2820f2e0955bec072a
Luís Henrique Braga de Sousa <agarb@pop-os.localdomain>
2022-03-22 13:20:44 -0300

    FEAT: Add sign up repository

--- src/controllers/signUpController.js
+++ src/controllers/signUpController.js
@@ -1,21 +1,18 @@
 import bcrypt from 'bcrypt';
+import { signUpRepository } from '../repositories/signUpRepository.js';
 
 export async function signUp(request, response) {
     const user = request.body;
   
     try {
-      const existingUsers = await connection.query('SELECT * FROM users WHERE email=$1', [user.email]);
+      const existingUsers = await signUpRepository.findUser(user.email);
       if (existingUsers.rowCount > 0) {
         return response.sendStatus(409);
       }
   
       const passwordHash = bcrypt.hashSync(user.password, 10);
   
-      await connection.query(`
-        INSERT INTO 
-          users(email, password, username, picture) 
-        VALUES ($1, $2, $3, $4)
-      `, [user.email, passwordHash, user.username, user.url])
+      await signUpRepository.insertUser(user.email, passwordHash, user.username, user.url);
   
       response.sendStatus(201);
     } catch (error) {
