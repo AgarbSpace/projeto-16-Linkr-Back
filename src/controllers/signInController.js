c8d15b958b4af6c99cc1c09aa7b51f352e5ead73
João Pedro Vitor de Oliveira <joao.pvo@aluno.ufop.edu.br>
2022-03-23 02:20:05 -0300

    Fix/ bug

--- src/controllers/signInController.js
+++ src/controllers/signInController.js
@@ -1,6 +1,6 @@
-import bcrypt from 'bcrypt';
-import { v4 as uuid } from 'uuid';
-import {signInRepository} from '../repositories/signInRepository.js';
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
+import { signInRepository } from "../repositories/signInRepository.js";
 
 export async function signIn(req, res) {
   const { email, password } = req.body;
@@ -10,16 +10,15 @@ export async function signIn(req, res) {
     if (!user.rows[0]) {
       res.sendStatus(401);
     }
-    
-    if (bcrypt.compareSync(password, user.rows[0].passwordHash)){
-        const token = uuid();
-        const data = {token: token, userId: user.rows[0].id}
-        await signInRepository.insertToken(token, user.rows[0].id)
-        res.send(data)
+
+    if (bcrypt.compareSync(password, user.rows[0].passwordHash)) {
+      const token = uuid();
+      const data = { token: token, userId: user.rows[0].id };
+      await signInRepository.insertToken(token, user.rows[0].id);
+      res.send(data);
     }
   } catch (error) {
     console.log(error);
     return response.sendStatus(500);
   }
-  res.sendStatus(401);
 }
