3b649eaf3c06b0851120db4b2365ea169a568323
Luís Henrique Braga de Sousa <agarb@pop-os.localdomain>
2022-03-24 20:21:11 -0300

    FEAT: Add timeline route and functions

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ import signUpRouter from "./signUpRouter.js";
 import signInRouter from './SignInRouter.js';
 import publicationRouter from "./publicationRouter.js";
 import headerRouter from "./headerRouter.js";
+import timelineRouter from "./timelineRouter.js";
 
 const router = Router();
 router.use(signUpRouter);
-router.use(signInRouter);
+router.use(signInRouter);;
 router.use(publicationRouter)
-router.use(headerRouter)
+router.use(headerRouter);
+router.use(timelineRouter);
 
 export default router;
