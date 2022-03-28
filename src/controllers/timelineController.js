48b334bb4eb2790559ee9e1c888cbb6e560066f6
deboracaires <deboracaires@pop-os.localdomain>
2022-03-27 21:02:08 -0300

    ADD: feat go to user page

--- src/controllers/timelineController.js
+++ src/controllers/timelineController.js
@@ -28,3 +28,34 @@ export async function getTimeline(request, response){
     }
         
 }
+
+export async function getTimelineByUserId (req, res) {
+  try {
+    const id = req.params.id;
+    console.log(id)
+    const posts = await timelineRepository.getPostsByUserId(req.params.id);
+    const post = [];
+    
+    for(const [idx, postsArray] of posts.userPosts.entries()){
+      const link = await urlMetadata(postsArray.link);
+      post.push({
+        id: postsArray.id,
+        userId: postsArray.userId,
+        image: link.image,
+        description: link.description,
+        title: link.title,
+        source: link.source,
+        text: postsArray.text,
+        picture: postsArray.picture,
+        username: postsArray.userName
+      });
+    }
+
+    const result = { username: posts.username, posts: post.reverse().slice(0,20)}
+    console.log(result)   
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+}
--- src/routes/timelineRouter.js
+++ src/routes/timelineRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getTimeline } from "../controllers/timelineController.js"
+import { getTimeline, getTimelineByUserId } from "../controllers/timelineController.js"
 import { validateTokenMiddleware } from "../middlewares/validateTokenMiddleware.js";
 
 const timelineRouter = Router();
 
-timelineRouter.get("/timeline",getTimeline);
+timelineRouter.get("/timeline", getTimeline);
+timelineRouter.get('/user/:id', getTimelineByUserId);
 
 export default timelineRouter;
