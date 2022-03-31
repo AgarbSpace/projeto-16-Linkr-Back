1092fb5955c4e1a534c94860ef00b53779f055ba
lostzilla <alanaa92@gmail.com>
2022-03-31 20:14:51 -0300

    first commit

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { searchUsers } from "../controllers/userController.js";
+import { searchUsers, getUserById } from "../controllers/userController.js";
 import { validateTokenMiddleware } from "../middlewares/validateTokenMiddleware.js";
 
 const userRouter = Router();
 
 userRouter.get('/searchusers', validateTokenMiddleware, searchUsers);
+userRouter.get("/users/:id", validateTokenMiddleware, getUserById)
 
 export default userRouter;
